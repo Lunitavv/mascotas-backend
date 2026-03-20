1833d7b4fbefee22eca840cf93446771450af0cc
Yahir Luna <salazaryahir818@gmail.com>
2026-03-20 07:55:48 -0600

    actualizacion19

---
+++ Mascotas/src/test/java/com/example/Mascotas/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.example.Mascotas.config; // Verifica que sea este el nombre de tu paquete
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:5173", // Puerto común de Vite
+                        "http://localhost:3000", // Puerto común de React
+                        "http://127.0.0.1:5173"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
