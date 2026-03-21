a7663ea1517bb112b3f956662629758fa946c17c
Yahir Luna <salazaryahir818@gmail.com>
2026-03-20 20:45:31 -0600

    actualizacion22

--- Mascotas/src/test/java/com/example/Mascotas/config/WebConfig.java
+++ Mascotas/src/test/java/com/example/Mascotas/config/WebConfig.java
@@ -6,18 +6,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        // Permitir CORS en todos los endpoints (/**)
         registry.addMapping("/**")
-                // Permitimos cualquier origen (Localhost y el futuro link de Netlify)
-                .allowedOriginPatterns("*")
-                // Permitimos los métodos necesarios
+                .allowedOriginPatterns("*") // <--- Esto es lo que falta en tu servidor actual
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                // Permitimos cualquier cabecera
                 .allowedHeaders("*")
-                // Permitir envío de credenciales (cookies, auth headers)
                 .allowCredentials(true);
     }
 }
